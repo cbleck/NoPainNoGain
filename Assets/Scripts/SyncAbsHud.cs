fc4a22df846ffd9ea1dfdf62c26ac5f5a7770645
cbleck <carlos@capptu.com>
2016-07-28 09:24:16 -0500

    1st commit

---
+++ Assets/Scripts/SyncAbsHud.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SyncAbsHud : MonoBehaviour {
+
+    public Image circleSync;
+    [Range(100.0f, 1000.0f)]
+    public float speed = 200.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        circleSync.fillAmount = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(circleSync.fillAmount < 1.0f)
+            circleSync.fillAmount += 0.005f * speed * Time.deltaTime;
+        else
+            circleSync.fillAmount = 0.0f;
+    }
+}
