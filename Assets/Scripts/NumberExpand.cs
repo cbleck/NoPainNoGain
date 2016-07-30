23451f4d6410b35a74ef86aac38a2851889abf76
cbleck <carlos@capptu.com>
2016-07-29 21:09:44 -0500

    sincronizar con numero

---
+++ Assets/Scripts/NumberExpand.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using SynchronizerData;
+using UnityEngine.UI;
+
+public class NumberExpand : MonoBehaviour {
+    private Animator anim;
+    private BeatObserver beatObserver;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        beatObserver = GetComponent<BeatObserver>();
+    }
+
+    void Update()
+    {
+        if ((beatObserver.beatMask & BeatType.UpBeat) == BeatType.UpBeat)
+        {
+            StartCoroutine("ExpandTextNGoNormal");
+        }
+    }
+
+    IEnumerator ExpandTextNGoNormal() {
+        GetComponent<Text>().fontSize = 50;
+        yield return new  WaitForSeconds(0.1f);
+
+        GetComponent<Text>().fontSize = 30;
+    }
+}
