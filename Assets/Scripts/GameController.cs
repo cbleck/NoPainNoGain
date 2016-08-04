735b41e53dddca141b8871f8b21142233734222e
cbleck <carlos@capptu.com>
2016-08-04 01:50:04 -0500

    gamplay ui

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,17 +7,20 @@ public class GameController : MonoBehaviour {
 
 	public Text accuracyText;
 
-    private ACCURACY_STATE current_state;
+    private AccuracyState current_tempstate, current_hit;
     private BeatObserver beatObserver;
 
 	private bool blockCurrentHit;
-	private string[] accuracyArray = { "Great", "Good", "Ok", "Miss" };
+	private string[] accuracyArray = { "Awesome","Great", "Good", "Not Bad", "Ok", "Miss", "NA" };
 
-    enum ACCURACY_STATE {
-        GREAT,
+	enum AccuracyState {
+        AWESOME,
+		GREAT,
         GOOD,
+		NOTBAD,
         OK,
-        MISS
+		MISS,
+		NOTASSIGNED
     }
 
 #if UNITY_IOS || UNITY_ANDROID
@@ -27,19 +30,32 @@ public class GameController : MonoBehaviour {
 
     void Start() {
         beatObserver = GetComponent<BeatObserver>();
-        current_state = ACCURACY_STATE.MISS;
+		current_tempstate = AccuracyState.AWESOME;
+		current_hit = AccuracyState.NOTASSIGNED;
 		blockCurrentHit = false;
     }
     // Update is called once per frame
     void Update () {
 
-        if ((beatObserver.beatMask & BeatType.UpBeat) == BeatType.UpBeat) {
-            StartCoroutine("RunCurrentAccuracy");
-        }
-        #if UNITY_STANDALONE
+		if ((beatObserver.beatMask & BeatType.OnBeat) == BeatType.OnBeat) {
+			Debug.Log ("VA");
+			blockCurrentHit = false;
+			StartCoroutine ("RunCurrentAccuracy");
+		}
+		if ((beatObserver.beatMask & BeatType.OffBeat) == BeatType.OffBeat) {
+
+			if (current_hit == AccuracyState.NOTASSIGNED) {
+				current_tempstate = AccuracyState.AWESOME;
+				StartCoroutine ("ShowMissText");
+				blockCurrentHit = true;
+			}
+			Debug.Log ("TERMINA_B");
+		}
+#if UNITY_STANDALONE
 
 		if (Input.GetKeyDown(KeyCode.Space) && !blockCurrentHit) {
 			blockCurrentHit = true;
+			current_hit = current_tempstate;
 			StartCoroutine("ShowAccuracyText");
         }
 
@@ -56,19 +72,39 @@ public class GameController : MonoBehaviour {
 
     IEnumerator RunCurrentAccuracy()
     {
-		blockCurrentHit = false;
-        current_state = ACCURACY_STATE.GREAT;
-        yield return new WaitForSeconds(0.3f);
-        current_state = ACCURACY_STATE.GOOD;
-        yield return new WaitForSeconds(0.3f);
-        current_state = ACCURACY_STATE.OK;
-        yield return new WaitForSeconds(0.3f);
-        current_state = ACCURACY_STATE.MISS;
+		current_hit = AccuracyState.NOTASSIGNED;
+		current_tempstate = AccuracyState.GOOD;
+		yield return new WaitForSeconds(0.05f);
+		current_tempstate = AccuracyState.GREAT;
+		yield return new WaitForSeconds(0.05f);
+		current_tempstate = AccuracyState.AWESOME;
+		yield return new WaitForSeconds(0.04f);
+		current_tempstate = AccuracyState.GREAT;
+        yield return new WaitForSeconds(0.05f);
+		current_tempstate = AccuracyState.GOOD;
+        yield return new WaitForSeconds(0.05f);
+		current_tempstate = AccuracyState.NOTBAD;
+		yield return new WaitForSeconds(0.07f);
+		current_tempstate = AccuracyState.OK;
+
+		Debug.Log ("TERMINA_A");
     }
 
 	IEnumerator ShowAccuracyText(){
 
-		accuracyText.text = accuracyArray[(int)current_state];
+		accuracyText.text = accuracyArray[(int)current_tempstate];
+		accuracyText.enabled = true;
+		yield return new WaitForSeconds (0.1f);
+		accuracyText.fontSize = 50;
+		yield return new WaitForSeconds (0.1f);
+		accuracyText.fontSize = 30;
+		yield return new WaitForSeconds (0.1f);
+		accuracyText.enabled = false;
+	}
+
+	IEnumerator ShowMissText(){
+
+		accuracyText.text = accuracyArray[(int)AccuracyState.MISS];
 		accuracyText.enabled = true;
 		yield return new WaitForSeconds (0.1f);
 		accuracyText.fontSize = 50;
