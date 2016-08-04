115468a41f3b6ee063e1090053ba921dcfb4cb04
cbleck <carlos@capptu.com>
2016-08-03 22:11:45 -0500

    changeColor

---
+++ Assets/Scripts/MenuChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuChanger : MonoBehaviour {
+
+	public Color initialBackgroundColor;
+	public Color finalBackgroundColor;
+	public float speed = 10;
+	private float startTime;
+
+	void Start(){
+		startTime = Time.time;
+	}
+	void Update(){
+		changeColor ();
+	}
+
+	private void changeColor(){
+
+		transform.GetComponent<Image> ().color = Color.Lerp (
+			initialBackgroundColor,
+			finalBackgroundColor,
+			(Time.time - startTime) *  speed );
+
+	}
+}
